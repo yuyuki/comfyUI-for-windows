0dca5a91f6a12f6118913054f0ff5eea6b3b02cc
Jaufré Devosse <djaufre@gmail.com>
2026-09-13 11:10:47 +0200

    Update download llm script: remove obsolete model downloads and adjust node positions in ExtractChapter workflow

--- Scripts/download llm.ps1
+++ Scripts/download llm.ps1
@@ -70,15 +70,9 @@ $outDir = "ComfyUI\models\text_encoders"
 
 Download-File -Url "https://huggingface.co/HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf" -OutDir $outDir
 
-Download-File -Url "https://huggingface.co/HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q6_K.gguf" -OutDir $outDir
-
-Download-File -Url "https://huggingface.co/HauhauCS/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive/resolve/main/Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q8_0.gguf" -OutDir $outDir
-
 Download-File -Url "https://huggingface.co/petruhonk/Qwen3.8-9B-Distill-uncensored-heretic-GGUF/resolve/main/Qwen3.8-9B-Distill-Heretic-Uncensored-Q8_0.gguf" -OutDir $outDir
 
 Download-File -Url "https://huggingface.co/Comfy-Org/Qwen-Image_ComfyUI/resolve/main/split_files/text_encoders/qwen_2.5_vl_7b_fp8_scaled.safetensors" -OutDir $outDir
-
-Download-File -Url "https://huggingface.co/llmfan46/Mistral-Small-3.2-24B-Instruct-2506-ultra-uncensored-heretic-GGUF/resolve/main/Mistral-Small-3.2-24B-Instruct-2506-ultra-uncensored-heretic-Q5_K_M.gguf" -OutDir $outDir
 
 # =================================================================== Clip
 Write-Host "=== Download Clip ===" -ForegroundColor Cyan
@@ -130,12 +124,6 @@ $sourceParent = Resolve-Path "$PSScriptRoot\.."
 
 $sourceFile = Join-Path $sourceParent "ComfyUI\models\clip\Z-Image-Engineer-V6-Q8_0.gguf"
 Copy-ModelToLMStudio -SourceFile $sourceFile -FolderName "Z-Image"
-
-$sourceFile = Join-Path $sourceParent "ComfyUI\models\text_encoders\Qwen3.5-9B-Uncensored-HauhauCS-Aggressive-Q6_K.gguf"
-Copy-ModelToLMStudio -SourceFile $sourceFile -FolderName "Qwen"
-
-$sourceFile = Join-Path $sourceParent "ComfyUI\models\text_encoders\Mistral-Small-3.2-24B-Instruct-2506-ultra-uncensored-heretic-Q5_K_M.gguf"
-Copy-ModelToLMStudio -SourceFile $sourceFile -FolderName "Qwen"
 
 $sourceFile = Join-Path $sourceParent "ComfyUI\models\text_encoders\Qwen3.8-9B-Distill-Heretic-Uncensored-Q8_0.gguf"
 Copy-ModelToLMStudio -SourceFile $sourceFile -FolderName "Qwen"
